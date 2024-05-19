32b9de01c43330cf823553edaf8268073e59abc1
KogaIJ <150610187+KogaIJ@users.noreply.github.com>
2024-05-19 13:55:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+
 const pickRandomItem = (dirPath) => {
   const items = fs.readdirSync(dirPath, { withFileTypes: true });
   const randomIndex = Math.floor(Math.random() * items.length);
   const randomItem = items[randomIndex];
+
   if (randomItem.isFile()) {
+    const randomImagePath = path.join(dirPath, randomItem.name);
+    document.getElementById('randomImage').src = randomImagePath;
     console.log(`Randomly picked file: ${randomItem.name}`);
   } else if (randomItem.isDirectory()) {
     console.log(`Randomly picked folder: ${randomItem.name}`);
     const folderPath = path.join(dirPath, randomItem.name);
-    const filesInFolder = fs.readdirSync(folderPath, { withFileTypes: true });
-    if (filesInFolder.length > 0) {
-      const randomFileIndex = Math.floor(Math.random() * filesInFolder.length);
-      const randomFile = filesInFolder[randomFileIndex];
-      console.log(`Randomly picked file inside folder: ${randomFile.name}`);
-    } else {
-      console.log("Folder is empty");
-    }
+    pickRandomItem(folderPath); // Pick a random item from the sub-folder
   }
 };
+
 const items2Folder = path.join(__dirname, 'items', 'items2');
 pickRandomItem(items2Folder);
